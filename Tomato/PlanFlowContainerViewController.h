d7da8b066233d0312cfd71614282a54a5e0791f8
qiushuitian <qiushuitian1111@126.com>
2014-11-23 12:01:33 +0800

    container view controller

---
+++ Tomato/PlanFlowContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlanFlowContainerViewController.h
+//  Tomato
+//
+//  Created by qiushuitian on 23/11/14.
+//  Copyright (c) 2014 dengjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlanFlowContainerViewController : UIViewController
+
+@end
---
+++ Tomato/PlanFlowContainerViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlanFlowContainerViewController.m
+//  Tomato
+//
+//  Created by qiushuitian on 23/11/14.
+//  Copyright (c) 2014 dengjian. All rights reserved.
+//
+
+#import "PlanFlowContainerViewController.h"
+
+@interface PlanFlowContainerViewController ()
+
+@end
+
+@implementation PlanFlowContainerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
